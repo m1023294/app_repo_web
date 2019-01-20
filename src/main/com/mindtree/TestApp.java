06edab9f3a4276fe3c9142df8b787e771f1a6e1b
Niranjan Shrishail Hampannavar <M1023294@mindtree.com>
2019-01-20 09:09:48 +0530

    commit

--- src/main/com/mindtree/TestApp.java
+++ src/main/com/mindtree/TestApp.java
@@ -3,7 +3,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestApp {
-	public List<String> getNames(){
+	public List<String> getNames1(){
+		List<String> list=new ArrayList<String>();
+		list.add("Niranjan");
+		list.add("Bhanu");
+		list.add("Vishwa");
+		System.out.println(list);
+		return list;
+	}
+	public List<String> getNames2(){
+		List<String> list=new ArrayList<String>();
+		list.add("Niranjan");
+		list.add("Bhanu");
+		list.add("Vishwa");
+		System.out.println(list);
+		return list;
+	}
+	public List<String> getNames3(){
+		List<String> list=new ArrayList<String>();
+		list.add("Niranjan");
+		list.add("Bhanu");
+		list.add("Vishwa");
+		System.out.println(list);
+		return list;
+	}
+	public List<String> getNames4(){
+		List<String> list=new ArrayList<String>();
+		list.add("Niranjan");
+		list.add("Bhanu");
+		list.add("Vishwa");
+		System.out.println(list);
+		return list;
+	}
+	public List<String> getNames5(){
 		List<String> list=new ArrayList<String>();
 		list.add("Niranjan");
 		list.add("Bhanu");
--- src/test/com/mindtree/TestAppTest.java
+++ src/test/com/mindtree/TestAppTest.java
@@ -10,8 +10,26 @@ import main.com.mindtree.TestApp;
 public class TestAppTest {
 
 	@Test
-	public void testGetNames() {
+	public void testGetNames1() {
 		TestApp testApp=new TestApp();
-		assertEquals(Arrays.asList("Niranjan", "Bhanu", "Vishwa"), testApp.getNames());
+		assertEquals(Arrays.asList("Niranjan", "Bhanu", "Vishwa"), testApp.getNames1());
+	}
+	
+	@Test
+	public void testGetNames2() {
+		TestApp testApp=new TestApp();
+		assertEquals(Arrays.asList("Niranjan", "Bhanu", "Vishwa"), testApp.getNames2());
+	}
+	
+	@Test
+	public void testGetNames3() {
+		TestApp testApp=new TestApp();
+		assertEquals(Arrays.asList("Niranjan", "Bhanu", "Vishwa"), testApp.getNames3());
+	}
+	
+	@Test
+	public void testGetNames4() {
+		TestApp testApp=new TestApp();
+		assertEquals(Arrays.asList("Niranjan", "Bhanu", "Vishwa"), testApp.getNames4());
 	}
 }	
